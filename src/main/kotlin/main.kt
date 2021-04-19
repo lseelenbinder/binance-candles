65d3f0f7fe470ad3655cb8b9866c0b81ccd1fa22
Luke Seelenbinder <me@lukeseelenbinder.com>
2021-04-19 23:25:51 +0200

    A bit more cleanup.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,5 @@
-import com.binance.api.client.BinanceApiCallback
 import com.binance.api.client.BinanceApiClientFactory
-import com.binance.api.client.domain.event.CandlestickEvent
 import com.binance.api.client.domain.market.CandlestickInterval
-import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
   val clientFactory = BinanceApiClientFactory.newInstance("", "")
@@ -13,49 +10,21 @@ fun main(args: Array<String>) {
   restClient.ping()
   println("success.")
 
+  val pair = "ethbtc"
+
   val serverTime = restClient.exchangeInfo.serverTime
   println("Binance server time: $serverTime")
 
   println("Fetching candles . . .")
   val manager = CandleManager()
 
-  val candleClosable = wsClient.onCandlestickEvent("ethbtc", CandlestickInterval.ONE_MINUTE, manager)
+  val closeHandle = wsClient.onCandlestickEvent(pair, CandlestickInterval.ONE_MINUTE, manager)
+
+  // TODO: determine multi-threadedness / race conditions
+
   Thread.sleep(1000 * 120)
 
-  candleClosable.close()
+  closeHandle.close()
   manager.finalize()
   println("All done.")
-}
-
-class CandleManager(
-  private val candles: MutableList<Candle>,
-  private val writer: CandleWriter,
-): BinanceApiCallback<CandlestickEvent> {
-  constructor() : this(
-    mutableListOf(),
-    CandleWriter("test.log"),
-  ) {}
-
-  override fun onResponse(candle: CandlestickEvent?) {
-    if (candle != null) {
-      if (candle.barFinal) {
-        // candle is closed, add to list
-        val candle = Candle(candle)
-        this.candles.add(candle)
-        this.writer.writeCandle(candle)
-        println("Candle ${this.candles.size} close: ${candle.closeTime} @ ${candle.close}")
-      } else {
-        Candle(candle)
-        println("Intermediate candle: ${candle.close}")
-      }
-    }
-  }
-
-  override fun onFailure(cause: Throwable?) {
-    println("Error encountered: $cause")
-  }
-
-  fun finalize() {
-    writer.finish()
-  }
 }
